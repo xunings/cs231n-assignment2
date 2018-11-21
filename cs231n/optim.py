bc2d53396884160f5d0572bb2a30878b35ed22f8
xunings <arc.neco@gmail.com>
2018-11-21 16:59:50 +0800

    Q1 until the best FC model.

--- cs231n/optim.py
+++ cs231n/optim.py
@@ -65,7 +65,8 @@ def sgd_momentum(w, dw, config=None):
     # TODO: Implement the momentum update formula. Store the updated value in #
     # the next_w variable. You should also use and update the velocity v.     #
     ###########################################################################
-    pass
+    v = config['momentum'] * v - config['learning_rate'] * dw
+    next_w = w+v
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
@@ -99,7 +100,9 @@ def rmsprop(w, dw, config=None):
     # in the next_w variable. Don't forget to update cache value stored in    #
     # config['cache'].                                                        #
     ###########################################################################
-    pass
+    config['cache'] = config['decay_rate'] * config['cache'] \
+        + (1-config['decay_rate']) * np.square(dw)
+    next_w = w - config['learning_rate'] * dw / (np.sqrt(config['cache'])+config['epsilon'])
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
@@ -139,7 +142,13 @@ def adam(w, dw, config=None):
     # NOTE: In order to match the reference output, please modify t _before_  #
     # using it in any calculations.                                           #
     ###########################################################################
-    pass
+    config['t'] += 1
+    config['m'] = config['beta1'] * config['m'] + (1-config['beta1'])*dw
+    config['v'] = config['beta2'] * config['v'] + (1-config['beta2'])*np.square(dw)
+    m_unbias = config['m'] / (1 - config['beta1']**config['t'])
+    v_unbias = config['v'] / (1 - config['beta2']**config['t'])
+    next_w = w - config['learning_rate'] * m_unbias / ( np.sqrt(v_unbias) + config['epsilon'] )
+    
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
